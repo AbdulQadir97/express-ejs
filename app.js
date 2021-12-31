1816e2413391e749de1b7186ed3f74440ee68db6
AbdulQadir97 <aq201541@gmail.com>
2021-12-31 21:57:59 +0500

    commit

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 const express = require('express')
 const app = express()
+const path = require("path");
 
+app.use(express.static(path.join(__dirname, 'build')));
 
-app.set('view engine', 'ejs');
-
-app.get('/', (req, res) => {
-    res.render('pages/index');
-})
-app.get('/form', (req, res) => {
-    res.render('pages/form');
-})
++app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(process.env.PORT 
     || 3000)
